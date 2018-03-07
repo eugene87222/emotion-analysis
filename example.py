fd0c35d75dbb0bcab15238182de806d6180dee25
Eugene Yang <eugene87222@gmail.com>
2018-03-07 17:56:21 +0800

    Add files via upload

--- example.py
+++ example.py
@@ -4,9 +4,9 @@ import simpleNLP
 
 var = simpleNLP.simpleNLP(u'''如果知道最後還是會分手那麼之前還會選擇在一起嗎''')
 
-print (var.sentiments_baidu())  # 情緒為 positive 的機率 (SnowNLP)
+print (var.sentiments_snowNLP())  # 情緒為 positive 的機率 (SnowNLP)
 
-print (var.sentiments_snowNLP())  # 情緒為 positive 的機率 (BaiduNLP)
+print (var.sentiments_baidu())  # 情緒為 positive 的機率 (BaiduNLP)
 
 print (var.parse_words_snownlp()) # 用 snownlp 斷詞
 
