47e6aab61d682d28c020df873670d0ff579d835c
Eugene Yang <eugene87222@gmail.com>
2018-03-07 17:52:15 +0800

    Add files via upload

--- example.py
+++ example.py
@@ -1,9 +1,15 @@
-import emotion
+# coding: utf-8
 
-var = emotion.Emotion_analysis(u'我很開心，因為年終發很多')
+import simpleNLP
 
-print (var.sentiments_percent())  # 情緒為 positive 的機率
+var = simpleNLP.simpleNLP(u'''如果知道最後還是會分手那麼之前還會選擇在一起嗎''')
 
-print (var.snownlp_parse_words()) # 用 snownlp 斷詞
+print (var.sentiments_baidu())  # 情緒為 positive 的機率 (SnowNLP)
 
-print (var.jieba_parse_words())   # 用 jieba 斷詞
+print (var.sentiments_snowNLP())  # 情緒為 positive 的機率 (BaiduNLP)
+
+print (var.parse_words_snownlp()) # 用 snownlp 斷詞
+
+print (var.parse_words_baidu())   # 用 baidu 斷詞
+
+print (var.parse_words_jieba())   # 用 jieba 斷詞
--- simpleNLP.py
+++ simpleNLP.py
@@ -5,22 +5,37 @@ warnings.filterwarnings('ignore') # 忽略警告訊息 (warning)
 
 import jieba    # 結巴分詞
 import opencc   # 繁簡體轉換
+from aip import AipNlp       # 百度NLP
 from snownlp import SnowNLP  # 情緒分析
 
-class Emotion_analysis:
+APP_ID = '10892513'
+API_KEY = '0gnO2IciyrzPKWvN16FKoxeD'
+SECRET_KEY = 'qeoiLP8dNEA7jolzcjD9AP9zqAiDR6H2'
+client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
+
+class simpleNLP:
 	def __init__(self, text):
 		cc = opencc.OpenCC('mix2s')
 		self.text_s = cc.convert(text)
 		self.text_t = text
 
-	def sentiments_percent(self):
-		s = SnowNLP(self.text_s)
-		return s.sentiments
+	def sentiments_snowNLP(self):
+		res = SnowNLP(self.text_s)
+		return res.sentiments
 
-	def snownlp_parse_words(self):
-		s = SnowNLP(self.text_s)
-		return s.words
+	def sentiments_baidu(self):
+		res = client.sentimentClassify(self.text_s)
+		return res['items'][0]['positive_prob']
 
-	def jieba_parse_words(self):
+	def parse_words_snownlp(self):
+		res = SnowNLP(self.text_s)
+		return res.words
+
+	def parse_words_baidu(self):
+		res = client.lexer(self.text_s)
+		res = [each['item'] for each in res['items']]
+		return res
+
+	def parse_words_jieba(self):
 		jieba.set_dictionary('dict.txt.big')
 		return jieba.lcut(self.text_t, cut_all=False)
